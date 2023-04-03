38bfbf72e4aaab99b0d1b8351808fa189259f05d
musmanali123 <usman232429@gmail.com>
2023-04-03 21:29:59 +0500

    new

---
+++ index.js
@@ -0,0 +1,3 @@
+import { location,name ,myage} from "./module practice/index_modue.js";
+
+console.log('Check LOGS -->',location,name,myage());
---
+++ module practice/index_modue.js
@@ -0,0 +1,8 @@
+const name = "HEllow";
+const location = "Karachi";
+
+const myage = ()=>{
+    return 10
+}
+
+export {name,location,myage}
